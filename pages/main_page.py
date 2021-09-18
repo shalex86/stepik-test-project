6cab64ee87b8cb1a5f2d16a56d1c064bebecc73c
shalex86 <alex.sh86@mail.ru>
2021-09-19 01:25:05 +0800

    main_page.py changed

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,5 +2,5 @@ from .base_page import BasePage
 
 class MainPage(BasePage):
 	def go_to_login_page(self):
-		login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+		login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
 		login_link.click() 
