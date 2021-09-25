8621796964e8070df5743ad6aca2f3098ed81443
shalex86 <alex.sh86@mail.ru>
2021-09-26 02:25:28 +0800

    test_product_page.py: three tests added

--- pages/product_page.py
+++ pages/product_page.py
@@ -4,10 +4,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class ProductPage(BasePage):
-    def go_to_add_to_basket(self):
+    def go_to_add_to_basket(self, promo=False):
         login_link = self.browser.find_element(*ProductPageLocators.BASKET_LINK)
         login_link.click()
-        self.solve_quiz_and_get_code()
+        if promo:
+            self.solve_quiz_and_get_code()
 
     def compare_values_book_price(self):
         book = self.browser.find_element(*ProductPageLocators.BOOK_NAME_FORM)
@@ -17,3 +18,9 @@ class ProductPage(BasePage):
         WebDriverWait(self.browser, 5).until(
             EC.presence_of_element_located(ProductPageLocators.PRICE_ALERT))
         assert book.text == book_.text and price.text == price_.text, "Book's names is not ident, prices is not ident"
+
+    def is_not_show_message_success(self):
+        assert self.is_not_element_present(*ProductPageLocators.BOOK_NAME_ALERT) == True, 'The message_success is show'
+
+    def is_disappeared_message_success(self):
+        assert self.is_disappeared(*ProductPageLocators.BOOK_NAME_ALERT) == True, 'The message_success is not disappeared'
--- test_product_page.py
+++ test_product_page.py
@@ -3,17 +3,38 @@ import time
 import pytest
 
 def custom_format_link_string(param):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{}"
-	return link.format(param)
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{}"
+    return link.format(param)
 
 @pytest.mark.parametrize('link', 
-	[
-		custom_format_link_string(x) if x != 7 else pytest.param(custom_format_link_string(x),
-			marks=pytest.mark.xfail) for x in range(10)
-	])
+    [
+        custom_format_link_string(x) if x != 7 else pytest.param(custom_format_link_string(x),
+            marks=pytest.mark.xfail) for x in range(10)
+    ])
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
-    page.go_to_add_to_basket()
-    time.sleep(5)
+    page.go_to_add_to_basket(promo=True)
     page.compare_values_book_price()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_add_to_basket()
+    page.is_not_show_message_success()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    page = ProductPage(browser, link)
+    page.open()
+    page.is_not_show_message_success()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_add_to_basket()
+    page.is_disappeared_message_success()
