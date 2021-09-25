56dabcde914b41db31a1d1b9eda040dc73d3de45
shalex86 <alex.sh86@mail.ru>
2021-09-26 01:17:07 +0800

    test_product_page.py: First bug (task 4.3.4) marked as XFAIL

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,17 @@
 from pages.product_page import ProductPage
 import time
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+def custom_format_link_string(param):
+	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{}"
+	return link.format(param)
+
+@pytest.mark.parametrize('link', 
+	[
+		custom_format_link_string(x) if x != 7 else pytest.param(custom_format_link_string(x),
+			marks=pytest.mark.xfail) for x in range(10)
+	])
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_add_to_basket()
